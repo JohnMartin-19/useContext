ffb7ece2e58c835dc6c6074cf8360add2813c809
John <johnnkonge2020@gmail.com>
2023-11-05 09:51:55 +0300

    added react and its component

---
+++ context/src/context/paused.jsx
@@ -0,0 +1,17 @@
+import React, { createContext } from "react"
+
+// create the context object
+const PauseContext = createContext()
+
+// create the context provider (component)
+function PauseProvider({ children }) {
+    
+    return (
+        <PauseContext.Provider value={true}>
+            {children}
+        </PauseContext.Provider>
+    )
+}
+
+// export
+export { PauseContext, PauseProvider }
